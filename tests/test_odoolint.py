96942af4f73900b81afeeb6787203bd5b5a41aa6
Moisés López <moylop260@vauxoo.com>
2016-05-29 02:58:18 +0000

    ref

--- tests/test_odoolint.py
+++ tests/test_odoolint.py
@@ -4,14 +4,25 @@
 
 import os
 
+from openerp.modules.module import get_module_resource
 from openerp.tests.common import TransactionCase
 
-MANIFEST_FILES = ['__openerp__.py', '__terp__.py', '__odoo__.py']
+from getaddons import get_modules, is_module
+
+# MANIFEST_FILES = ['__openerp__.py', '__terp__.py', '__odoo__.py']
 
 
 class OdooLint(TransactionCase):
 
-    def get_xml_ids(self, modules=None):
+    def test_wrong_xml_ids(self):
+        module_paths = os.environ.get('TRAVIS_BUILD_DIR', '').split(",") + \
+            os.environ.get('TEST_OTHER_PROJECTS', '').split(",")
+        modules = []
+        for module_path in module_paths:
+            modules.extend(get_modules(module_path))
+        xml_ids = self.get_db_xml_ids(modules)
+
+    def get_db_xml_ids(self, modules=None):
         """Search all xml ids and identify the category from manifest file
         :params modules list: List of string with modules to filter.
             None for all modules.
@@ -22,18 +33,18 @@ class OdooLint(TransactionCase):
             xml_record.module + '.' + xml_record.name
             for xml_record in self.env['ir.module.data'].search(domain)]
 
-    def get_manifest_path(self, module):
-        """Get the path of a odoo module
-        :param module str: Name of module to get path
-        :return str: Full path of module
-        """
-        module_path = None  # TODO: Get module_path
-        manifest_path = None
-        for fname_manifest in MANIFEST_FILES:
-            manifest_path = os.path.join(module_path, fname_manifest)
-            if os.path.isfile(manifest_path):
-                return manifest_path
-        raise "Manifest don't found for module %s" % module
+    # def get_manifest_path(self, module):
+    #     """Get the path of a odoo module
+    #     :param module str: Name of module to get path
+    #     :return str: Full path of module
+    #     """
+    #     module_path = get_module_resource(module)
+    #     manifest_path = None
+    #     for fname_manifest in MANIFEST_FILES:
+    #         manifest_path = os.path.join(module_path, fname_manifest)
+    #         if os.path.isfile(manifest_path):
+    #             return manifest_path
+    #     raise "Manifest don't found for module %s" % module
 
     def get_xml_ids_group_by_section(self, xml_ids):
         """Create a dictionary with section from manifest and xml_ids
@@ -42,9 +53,23 @@ class OdooLint(TransactionCase):
         """
         for module_xml_id in xml_ids:
             module, xml_id = module_xml_id.split('.')
-            manifest_path = self.get_manifest_path(module)
-            xml_id_section = self.xml_id_get_section(manifest_path, xml_id)
+            module_path = get_module_resource(module)
+            manifest_path = self.is_module(module_path)
+            xml_id_section = self.get_xml_id_section(manifest_path, xml_id)
         return xml_id_section
+
+    def get_xml_id_section(self, manifest_path, xml_id):
+        module_dirname = os.path.dirname(manifest_path)
+        sections = ['data', 'test', 'demo']
+        manif_dict = eval(open(manifest_path).read())
+        for root, _, filenames in os.walk(module_dirname, followlinks=True):
+            for filename in filenames:
+                fname = os.path.join(root, filename)
+                fname_r = os.path.relpath(fname, module_dirname)
+                if xml_id in open(fname).read():
+                    for section in sections:
+                        if fname_r in manif_dict.get(section, []):
+                            return section
 
     def test_default_company(self):
         xml_ids = self.get_xml_ids()
