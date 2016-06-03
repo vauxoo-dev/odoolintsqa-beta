5b57c2d930ea21f1a7b89fa7bdf7f170338709dd
Moisés López <moylop260@vauxoo.com>
2016-06-03 16:09:18 +0000

    get dependencies tree

--- odoolint/models/ir_model_data.py
+++ odoolint/models/ir_model_data.py
@@ -44,6 +44,8 @@ class IrModelData(models.Model):
 
     @api.multi
     def _check_data_ref_demo(self):
+        """Check data where a xml_id of section demo or test is referenced
+        """
         self.ensure_one()
         imd_new = get_file_info()
         if self.section in ['demo', 'demo_xml', 'test'] and \
@@ -57,8 +59,66 @@ class IrModelData(models.Model):
             return False
         return True
 
+    @api.multi
+    def _get_module_upstream_dependencies(self, module_ids, known_dep_ids=None,
+        exclude_states=['installed', 'uninstallable', 'to remove'],
+        context=None):
+        """Copied from odoo native ir.module.module v9.0
+        Return the dependency tree of modules of the given `ids`, and that
+        satisfy the `exclude_states` filter """
+        # TODO: Move a ir_module_module.py
+        ids = module_ids
+        cr = self.env.cr
+        uid = self.env.uid
+        if not ids:
+            return []
+        known_dep_ids = set(known_dep_ids or [])
+        cr.execute('''SELECT DISTINCT m.id
+                        FROM
+                            ir_module_module_dependency d
+                        JOIN
+                            ir_module_module m ON (d.module_id=m.id)
+                        WHERE
+                            m.name IN (SELECT name from ir_module_module_dependency where module_id in %s) AND
+                            m.state NOT IN %s AND
+                            m.id NOT IN %s ''',
+                   (tuple(ids), tuple(exclude_states), tuple(known_dep_ids or ids)))
+        new_dep_ids = set([m[0] for m in cr.fetchall()])
+        missing_mod_ids = new_dep_ids - known_dep_ids
+        known_dep_ids |= new_dep_ids
+        if missing_mod_ids:
+            known_dep_ids |= set(self._get_module_upstream_dependencies(list(missing_mod_ids),
+                                                              known_dep_ids, exclude_states, context))
+        return list(known_dep_ids)
+
+    @api.multi
+    def _check_xml_id_unachievable(self):
+        """Check a unachievable xml_id referenced
+        """
+        self.ensure_one()
+        module = self.env['ir.module.module']
+        imd_new = get_file_info()
+        module_curr_str = imd_new.get('module_real')
+        module_ref_str = self.module_real
+        if not module_curr_str or not module_ref_str or \
+                module_ref_str == module_curr_str:
+            return True
+        module_curr = module.search([('name', '=', module_curr_str)], limit=1)
+        module_curr_dep_ids = self._get_module_upstream_dependencies(
+            module_curr.ids, exclude_states=['uninstallable', 'to remove'])
+        module_curr_deps = module.browse(module_curr_dep_ids).mapped('name')
+        if module_curr_deps and module_ref_str not in module_curr_deps:
+            _logger.warning(
+                "The xml_id '%s.%s' is unachievable.",
+                self.module_real, self.name,
+            )
+            return False
+        return True
+
+
     @tools.ormcache(skiparg=3)
     def xmlid_lookup(self, cr, uid, xmlid):
         res = super(IrModelData, self).xmlid_lookup(cr, uid, xmlid)
         self._check_data_ref_demo(cr, uid, res[0])
+        self._check_xml_id_unachievable(cr, uid, res[0])
         return res
